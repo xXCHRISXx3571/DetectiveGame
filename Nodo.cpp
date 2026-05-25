869fddee87980bf499e909861d18df9b7a3b65ba
Haipro2526 <julian.arc2601@gmail.com>
2026-05-25 07:09:42 -0500

    add detective,cpp

--- Nodo.cpp
+++ Nodo.cpp
@@ -1,9 +1,14 @@
 #include "Nodo.h"
+#include "Pista.h"
 
 Nodo::Nodo() {
 
     contenido = 'o';
-
+    descubierto = false;
+    tienePista = false;
+    tieneTestigo = false;
+    pista = nullptr;
+    
     arriba = nullptr;
     abajo = nullptr;
     izquierda = nullptr;
