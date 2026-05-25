ffa9219775172eda8e9ff01ce270438c66291ead
Haipro2526 <julian.arc2601@gmail.com>
2026-05-24 19:22:11 -0500

    clase pista

--- Pista.cpp
+++ Pista.cpp
@@ -27,3 +27,32 @@ char Pista::getSimboloTablero() {
     }
 }
 
+string Pista::getNombre() {
+    switch (tipo) {
+        case HUELLA:
+            return "Huella";
+        case COARTADA:
+            return "Coartada";
+        case TESTIMONIO:
+            return "Testimonio";
+        case PRUEBA_FORENSE:
+            return "Prueba Forense";
+        default:
+            return "Desconocida";
+    }
+}
+
+string Pista::getDescriocionEfecto() {
+    switch (tipo) {
+        case HUELLA:
+            return "Deja una huella digital en el lugar del crimen";
+        case COARTADA:
+            return "Prueba de presencia en el lugar del crimen";
+        case TESTIMONIO:
+            return "Información obtenida de un testigo";
+        case PRUEBA_FORENSE:
+            return "Evidencia científica recolectada del lugar del crimen";
+        default:
+            return "";
+    }
+}
